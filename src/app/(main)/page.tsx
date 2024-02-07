932903d28a2a863a9562f07d52ec8438415b5688
Vishnu Kulkarni <vishnu@starapps.studio>
2024-02-08 00:18:11 +0530

    tiptap  minimal setuo

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,14 @@
 import LeftSidebar from "@/components/global/Sidebar";
+import Tiptap from "@/components/tiptap/Tiptap";
 import React from "react";
 
 const page = () => {
   return (
-    <div className=" flex justify-between items-center w-full h-full">
+    <div className=" flex  justify-start gap-3 items-center w-full h-full">
       <LeftSidebar />
+      <div className=" bg-white text-black">
+        <Tiptap />
+      </div>
     </div>
   );
 };
